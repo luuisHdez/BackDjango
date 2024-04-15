7b3ad5f493f0d5a5aa9e09afd5bb6c89cbc7684a
Luis Hernandez <luiszs.lr@gmail.com>
2024-04-14 21:35:31 -0600

    file urls.py modify to add register.view

--- tasks/urls.py
+++ tasks/urls.py
@@ -14,5 +14,7 @@ urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('docs/', include_docs_urls(title='Tasks API'))
+    path('docs/', include_docs_urls(title='Tasks API')),
+# Adding Path to register module
+    path('api/v1/register/', RegisterUser.as_view(), name='register_user')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
-
+ 
 from .serializer import TaskSerializer
 from .models import Task
 
@@ -11,3 +11,23 @@ class TaskView(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+
+class RegisterUser(APIView):
+    def post(self, request, *args, **kwargs):
+	username = request.data.get('username', '')
+	password = request.data.get('password', '')
+	email = request.data.get('email', '')
+
+	if not username o not password or not email:		
+	    return Response({'error': 'Username, password and email are required'}, status=status.HTTP_400_BAD_REQUEST)
+
+	if User.object.filter(username= username).exists():
+            return Response({'error': 'Username is already in use'}, status=status.HTTP_400_BAD_REQUEST)
+
+	# Here need to add validations for password, remember
+	
+	user = User.objects.create_user(username=username, email=email)
+	user.set_password(password)
+	user.save()
+
+	return Response({'detail': 'User created successfully'}, status=status.HTTP_201_CREATED)
